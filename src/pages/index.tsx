da302539c3c62c3b3c91862bf821bc0e0f5a0e80
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-05-03 12:11:47 -0300

    gerando a home de forma estatica

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+//SSG - Fica estatico melhorando a performace. 
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return { 
+    props: {
+      episodes: data, 
+    },
+    // revalidate recebe um numero em segundo, avisando em quanto tempo quer uma nova versao da pagina
+    revalidate: 60 * 60 * 8 //a cada 8 horas uma nova atualização acontece
+  }
 }
