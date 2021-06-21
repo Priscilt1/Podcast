c9bf859a257c4d7b4477cbb479f5a10d5919a79f
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-21 16:58:15 -0300

    feat: :construction: create context player

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -1,15 +1,18 @@
 import { createContext } from 'react'
 
 type Episode = {
-  title: string,
-  members: string,
-  thumbnail: string,
-  duration: string,
-  url: string,
+  title: string
+  members: string
+  thumbnail: string
+  duration: number
+  url: string
 }
 
 type PlayerContextData = {
-  episodeList: Episode[];
+  episodeList: Episode[]
+  currentEpisodeIndex: number //apontando qual posição esta tocando o espisodio atualmente
+  play: (episode: Episode) => void //funcao retornando vazia
 }
 
-export const PlayerContext = createContext('') //o valor padrão define o formato dos dados que ira usar no contexto
+//o valor padrão define o formato dos dados que ira usar no contexto
+export const PlayerContext = createContext({} as PlayerContextData) //falando que é um array e pegando as mesmas estruturas
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,20 @@ import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 import styles from '../styles/app.module.scss'
 import { PlayerContext } from '../contexts/PlayerContext'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+
+  //funcao para manipular do estado
+  function play(episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+  }
+
   return (
-    <PlayerContext.Provider value={'Rugue'}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
